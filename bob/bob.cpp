f2ce41b4e3bfd94cff2828a93328b76041cef6f7
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2016-02-21 20:11:42 +0100

    bob: adding initial commit

---
+++ bob/bob.cpp
@@ -0,0 +1,35 @@
+#include "bob.h"
+#include <cstdio>
+#include <cctype>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+/*
+function to check if a string has an alphabetic character and is all
+uppercase. In case when the string contains no alphabets returns false
+*/
+bool all_upper(const string& s){
+    bool no_char = false;
+    for (const char &c: s){
+	if (islower(c))
+	    return false;
+	if (isalpha(c))
+	    no_char = true;
+    }
+    return no_char;
+}
+
+string bob::hey(string in){
+
+    boost::trim(in);
+    char last = in.back();
+    if (in == "")
+	return "Fine. Be that way!";
+    else if (all_upper(in))
+	return "Whoa, chill out!";
+    else if (last == '?')
+	return "Sure.";
+    else
+	return "Whatever.";
+}
---
+++ bob/bob.h
@@ -0,0 +1,10 @@
+#ifndef _BOB_H_
+#define _BOB_H_
+
+#include <string>
+
+namespace bob{
+    std::string hey(std::string input);
+}
+
+#endif /* _BOB_H_ */
